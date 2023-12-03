f114f26899711c601cad161e19dd0d30bbe40dac
samsja <sami.jaghouar@gmail.com>
2023-12-03 14:56:54 +0100

    add day 1 solution
    Signed-off-by: samsja <sami.jaghouar@gmail.com>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn read_file(path: String) -> std::io::Result<String> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn get_first_digit<I: Iterator<Item = char>>(chars: I) -> Result<usize, &'static str> {
+    for c in chars {
+        if c.is_ascii_digit() {
+            return Ok(c.to_digit(10).unwrap() as usize);
+        }
+    }
+
+    return Err("No digit found");
+}
+
+fn get_calib_line_value(input: &str) -> usize {
+    let left_digit = get_first_digit(input.chars()).unwrap();
+    let right_digit = get_first_digit(input.chars().rev().into_iter()).unwrap();
+
+    return left_digit * 10 + right_digit;
+}
+fn calib_sum_value(_inputs: Vec<&str>) -> usize {
+    return _inputs.into_iter().map(get_calib_line_value).sum();
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: my_program <file_name>");
+        std::process::exit(1);
+    }
+
+    let file_name = &args[1];
+    println!("The file name is: {}", file_name);
+
+    let bindings = read_file(file_name.to_string())?;
+    let contents: Vec<&str> = bindings.lines().collect();
+    let sum = calib_sum_value(contents);
+    println!("Calibration sum is: {}", sum);
+    Ok(())
 }
