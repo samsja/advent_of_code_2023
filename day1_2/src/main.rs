5a517d5ad47b8dbb9b1d9c9e7c662f243aed1e7e
samsja <sami.jaghouar@gmail.com>
2023-12-03 16:34:36 +0100

    add day 1 2
    Signed-off-by: samsja <sami.jaghouar@gmail.com>

--- day1_2/src/main.rs
+++ day1_2/src/main.rs
@@ -9,20 +9,27 @@ fn read_file(path: String) -> std::io::Result<String> {
     Ok(contents)
 }
 
-//one 3
-//two 3
-//six 3
+fn get_n_carac(chars: Vec<char>, i: usize, n: usize, reverse: bool) -> Option<String> {
+    let maybe_n_carac = chars.get(i..i + n);
 
-//four 4
-//five 4
-//nine 4
+    if maybe_n_carac.is_none() {
+        return None;
+    } else {
+        let n_caracs: String = if !reverse {
+            maybe_n_carac.unwrap().iter().collect()
+        } else {
+            maybe_n_carac.unwrap().iter().rev().collect()
+        };
+        return Some(n_caracs);
+    }
+}
 
-//three 5
-//seven 5
-//eight 5
-
-fn get_first_digit(input: &str, left: bool) -> Result<usize, String> {
-    let chars: Vec<char> = input.chars().collect();
+fn get_first_digit(input: &str, reverse: bool) -> Result<usize, String> {
+    let chars: Vec<char> = if reverse {
+        input.chars().rev().collect()
+    } else {
+        input.chars().collect()
+    };
 
     for i in 0..chars.len() {
         let carac = chars[i];
@@ -31,54 +38,40 @@ fn get_first_digit(input: &str, left: bool) -> Result<usize, String> {
             return Ok(carac.to_digit(10).unwrap() as usize);
         }
 
-        let maybe_three_carac = if left {
-            chars.get(i..i + 3)
-        } else {
-            chars.get(i - 3..i)
-        };
-        if maybe_three_carac.is_none() {
-            continue;
-        }
-        let three_carac = maybe_three_carac.unwrap().iter().collect::<String>();
-        match three_carac.as_str() {
-            "one" => return Ok(1),
-            "two" => return Ok(2),
-            "six" => return Ok(6),
-            _ => (),
+        let maybe_three_carac = get_n_carac(chars.clone(), i, 3, reverse);
+
+        match maybe_three_carac {
+            Some(three_carac) => match three_carac.as_str() {
+                "one" => return Ok(1),
+                "two" => return Ok(2),
+                "six" => return Ok(6),
+                _ => (),
+            },
+            None => (),
         }
 
-        let maybe_four_carac = if left {
-            chars.get(i..i + 4)
-        } else {
-            chars.get(i - 4..i)
-        };
-        if maybe_four_carac.is_none() {
-            continue;
-        }
-        let four_carac = maybe_four_carac.unwrap().iter().collect::<String>();
+        let maybe_four_carac = get_n_carac(chars.clone(), i, 4, reverse);
 
-        match four_carac.as_str() {
-            "four" => return Ok(4),
-            "five" => return Ok(5),
-            "nine" => return Ok(9),
-            _ => (),
+        match maybe_four_carac {
+            Some(four_carac) => match four_carac.as_str() {
+                "four" => return Ok(4),
+                "five" => return Ok(5),
+                "nine" => return Ok(9),
+                _ => (),
+            },
+            None => (),
         }
 
-        let maybe_five_carac = if left {
-            chars.get(i..i + 5)
-        } else {
-            chars.get(i - 5..i)
-        };
-        if maybe_five_carac.is_none() {
-            continue;
-        }
-        let five_carac = maybe_four_carac.unwrap().iter().collect::<String>();
+        let maybe_five_carac = get_n_carac(chars.clone(), i, 5, reverse);
 
-        match five_carac.as_str() {
-            "three" => return Ok(3),
-            "seven" => return Ok(7),
-            "eight" => return Ok(8),
-            _ => (),
+        match maybe_five_carac {
+            Some(five_carac) => match five_carac.as_str() {
+                "three" => return Ok(3),
+                "seven" => return Ok(7),
+                "eight" => return Ok(8),
+                _ => (),
+            },
+            None => (),
         }
     }
 
@@ -88,8 +81,8 @@ fn get_first_digit(input: &str, left: bool) -> Result<usize, String> {
 }
 
 fn get_calib_line_value(input: &str) -> usize {
-    let left_digit = get_first_digit(input, true).unwrap();
-    let right_digit = get_first_digit(input, false).unwrap();
+    let left_digit = get_first_digit(input, false).unwrap();
+    let right_digit = get_first_digit(input, true).unwrap();
 
     return left_digit * 10 + right_digit;
 }
@@ -104,7 +97,6 @@ fn main() -> std::io::Result<()> {
         eprintln!("Usage: cargo run <file_name>");
         std::process::exit(1);
     }
-
     let file_name = &args[1];
     let bindings = read_file(file_name.to_string())?;
     let contents: Vec<&str> = bindings.lines().collect();
