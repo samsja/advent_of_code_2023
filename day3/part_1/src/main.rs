f3226c5cdf0bfff5ec310101fa6ba0af3805d070
samsja <sami.jaghouar@gmail.com>
2023-12-10 14:07:46 +0100

    wip
    Signed-off-by: samsja <sami.jaghouar@gmail.com>

--- day3/part_1/src/main.rs
+++ day3/part_1/src/main.rs
@@ -2,35 +2,73 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn look_for_number(line: &str, i: usize) -> usize {
-    0
-}
-
 fn is_special(c: char) -> bool {
     return !(c.is_ascii_digit() || c == '.');
 }
 
 fn val_trio(top: &str, mid: &str, bottom: &str) -> usize {
-    let mut val: usize = 0;
+    /*
+    input like
+    467..114..
+    ...*......
+    ..35..633.
+     */
 
-    let mid_iter = ".".chars().chain(mid.chars()).chain(".".chars());
-    // add . before and after
-    for (i, c) in mid_iter.enumerate() {
-        if is_special(c) {
-            val += look_for_number(top, i - 1)
-                + look_for_number(bottom, i - 1)
-                + look_for_number(mid, i - 1)
+    let mid: Vec<char> = mid.chars().collect();
+    let top: Vec<char> = top.chars().collect();
+    let bottom: Vec<char> = bottom.chars().collect();
+
+    let mut sum: usize = 0;
+
+    let mut current_num: String = String::from("");
+
+    let mut special_carac_around = false;
+
+    for (i, &c) in mid.iter().enumerate() {
+        special_carac_around =
+            special_carac_around || is_special(c) || is_special(top[i]) || is_special(bottom[i]);
+
+        print!("{special_carac_around} ");
+        if c.is_ascii_digit() {
+            current_num.push(c);
+        } else {
+            if current_num.len() > 0 {
+                if special_carac_around {
+                    let s = current_num.parse::<usize>().unwrap();
+                    print!("{s} \n");
+                    sum += s;
+                }
+
+                special_carac_around = false;
+                current_num.clear();
+            }
         }
     }
 
-    val
+    sum
+}
+
+fn fake_concat_vec<'a>(input: &'a Vec<&str>, extra_line: &'a str, i: usize) -> &'a str {
+    if i == 0 || i == input.len() + 1 {
+        return extra_line;
+    } else {
+        return input[i - 1];
+    }
 }
 
 fn game_sum(input: Vec<&str>) -> usize {
     let mut sum: usize = 0;
+    let extra_line = ".".repeat(input[0].len());
+    let extra_line = extra_line.as_str();
 
-    for i in 1..input.len() - 1 {
-        sum += val_trio(input[i - 1], input[i], input[i + 1])
+    let expanded_input_len = 2 + input.len();
+
+    for i in 1..(expanded_input_len - 1) {
+        let top = fake_concat_vec(&input, extra_line, i - 1);
+        let mid = fake_concat_vec(&input, extra_line, i);
+        let bottom = fake_concat_vec(&input, extra_line, i + 1);
+
+        sum += val_trio(top, mid, bottom);
     }
 
     sum
